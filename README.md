fbe4fb3608e2643b0b26bf50ec6a68ec54411875
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-09-17 11:24:17 -1000

    results output to text

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-# Indeed.com-Job-Web-Scraper
+# Indeed.com Job Web-Scraper
 A python script automating job searching on Indeed.com
 
-
+Note: As of now this only works for US searches.
 
 ## **<u>Usage</u>**
 
 From command line, navigate to the main folder. 
 
-In the command line, use this command to search for jobs:  ```webscraper.py [City] [StateAbreviation] [SearchTerm(OPTIONAL)]```
+In the command line, use this command to search for jobs:  ```webscraper.py [City] [StateAbbreviation][NumberOfPagesToSearch][SearchTerm(OPTIONAL)]```
 
-For example: ```webscraper.py Portland OR c++```
+(A full list of state abbreviations is available here: <https://www.ssa.gov/international/coc-docs/states.html>)
+
+For example: ```webscraper.py Portland OR 5 c++``` will search 5 pages of job listings in Portland, Oregon, matching the search term "c++"
+
+You can view your search results in ```output.txt```
 
 
 
 Happy job hunting!
+
+
+
+
 
--- webscraper.py
+++ webscraper.py
@@ -6,52 +6,73 @@ from bs4 import BeautifulSoup
 
 def printResults():
     
-    print(title_elem.text.strip())
-    print(salary_elem.text.strip())
+    text_file.write('TITLE: ' + title_elem.text.strip() + '\n')
+    text_file.write('SALARY: ' + salary_elem.text.strip() + '\n')
+    text_file.write('REQUIREMENTS: ')
     for requirement_elem in requirements_elem_list:
         if None in (requirement_elem):
             continue
 
-        print(requirement_elem.text.strip())
-    print()
-    print()
+        text_file.write(requirement_elem.text.strip())
+    if(link_elem != None):
+            text_file.write('\n' + 'APPLY:')
+            text_file.write('www.indeed.com' + link_elem['href'].strip() + '\n')
+
+    text_file.write("\n \n")
 
 
-URL = sys.argv[1]
+State = sys.argv[1].upper()
+City = sys.argv[2].capitalize()
+number_of_pages_to_search = int(sys.argv[3])
 
+URL = 'https://www.indeed.com/l-' + City + ',-' + State + '-jobs.html'
 
-while(True):
+page_index = 10
+page_num = 0
+
+with open("Output.txt", "w") as text_file:
     
-    page = requests.get(URL)
+    while(page_num < number_of_pages_to_search):
+        text_file.write('RESULTS FOR PAGE ' + str(page_num) + '\n')
+        text_file.write('----------------------------------' + '\n')
+        page_num = page_num + 1
+        page = requests.get(URL)
 
-    if(page.status_code != 200):
-        break
-    soup = BeautifulSoup(page.content,'html.parser')
-    results = soup.find(id='resultsCol')
+        if(page.status_code != 200):
+            break
+        soup = BeautifulSoup(page.content,'html.parser')
+        results = soup.find(id='resultsCol')
 
-    job_elems = results.find_all('div', class_='jobsearch-SerpJobCard')
+        job_elems = results.find_all('div', class_='jobsearch-SerpJobCard')
 
-    for job_elem in job_elems:
-        title_elem = job_elem.find('h2', class_='title')
-        salary_elem = job_elem.find('div', class_='salarySnippet')
-        requirements_elem_list = job_elem.find_all('div', class_='jobCardReqItem')
+        for job_elem in job_elems:
+            title_elem = job_elem.find('h2', class_='title')
+
+
+            link_elem = title_elem.find('a',href = True)
         
-        if None in (title_elem, salary_elem,requirements_elem_list):
-            continue
 
-        if(len(sys.argv) > 2):
-            if(sys.argv[2].lower() in title_elem.text.lower()):
-                printResults()
+
+            salary_elem = job_elem.find('div', class_='salarySnippet')
+            requirements_elem_list = job_elem.find_all('div', class_='jobCardReqItem')
+        
+            if None in (title_elem, salary_elem,requirements_elem_list):
+                continue
+
+            if(len(sys.argv) > 4):
+                if(sys.argv[4].lower() in title_elem.text.lower()):
+                    printResults()
+                else:
+                    text_file.write('End of results found for: ' + sys.argv[4])
+                    break
             else:
-                print('End of results found for: ' + sys.argv[2])
-                break
-        else:
-            printResults()
-        
-        
-
-
+                printResults()
     
+        URL = 'https://www.indeed.com/jobs?q=&l=' + City + '%2C+' + State + '&start=' + str(page_index)
+        page_index = page_index + 10
+        
+        
+print('END OF FILE')
     
 
 
